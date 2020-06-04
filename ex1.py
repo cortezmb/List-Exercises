355725ed1f1cc223e46572b39955621ca716878a
cortezmb <michael.cortez78@gmail.com>
2020-06-04 10:04:04 -0500

    new method

--- ex1.py
+++ ex1.py
@@ -1,5 +1,12 @@
 #List exercise 1
-num = [81, 59, 32, 10, 12, 18]
+numList = [81, 59, 32, 10, 12, 18]
 
-sumNum = sum(num)
-print(sumNum)
+# sumNum = sum(numList)
+# print(sumNum)
+
+asum = 0
+
+for val in numList :
+    asum += val
+
+print(asum)
--- ex9.py
+++ ex9.py
@@ -1,29 +1,9 @@
 #Lists exercise 9
-# x = [[2, -2], [5, 3]]
+x = [[2, -2], [5, 3]]
 
-# for i in range(len(x)) :
-#     for j in range(len(x[i]) :
-#         y = x[i] + x[j]
+for i in range(len(x)) :
+    for j in range(len(x[i]) :
+        y = x[i] + x[j]
     
-# print(y)
-
-# outsideIndex = 0
-# innerIndex = 0
-
-# while outsideIndex < len(x) :
-#     while innerIndex < len(x[outsideIndex]) :
-#         output = x[outsideIndex][innerIndex]
-#         print(f"{output}")
-#         innerIndex += 1
-#     innerIndex = 0
-#     outsideIndex += 1
-
-a = [ [2, -2],  
-    [ 5, 3 ] ]  
-i = 0
-j = 0
-while i < len(a) :  
-    while j < len(a[i]) :  
-        sumNum = a[i] + a[j]
-        print(sumNum)
+print(y)
     
